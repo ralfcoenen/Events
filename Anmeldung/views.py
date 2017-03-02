4a96580a4137dbc9618ac7bf5df410135a3127f6
Ralf Coenen <ralf@subuthi.de>
2017-03-02 12:25:00 +0100

    Styles eingefügt

--- Anmeldung/views.py
+++ Anmeldung/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Anmeldung.models import Event, Teilnehmer
+
 
 def index(request):
     return HttpResponse("Willkommen zur Anmeldung.")
 
 def event_list(request):
-    return render(request, 'Anmeldung/event_list.html', {})
+    events = Event.objects.all()
+    return render(request, 'Anmeldung/event_list.html', {'events': events})
