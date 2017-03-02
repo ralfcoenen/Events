8a081997c7461433bc742ebc17414a6d7e2097dd
Ralf Coenen <ralf@subuthi.de>
2017-03-02 08:19:27 +0100

    Template Anmeldung verdrahtet

--- Anmeldung/views.py
+++ Anmeldung/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Willkommen zur Anmeldung.")
+
+def event_list(request):
+    return render(request, 'Anmeldung/event_list.html', {})
