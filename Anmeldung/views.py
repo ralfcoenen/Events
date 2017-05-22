6a2987fbcabb1144b6286dfeeef78ef7dc95b794
ralfcoenen <nomail@web.de>
2017-05-22 11:45:45 +0200

    Erste Ideen für ein Zählung

--- Anmeldung/views.py
+++ Anmeldung/views.py
@@ -70,6 +70,46 @@ def teilnehmer_neu(request, pk):
                 teilnehmer.event = event
                 teilnehmer.save()
                 messages.success(request, 'Neue Anmeldung erfolgreich durchgeführt!')
+                #
+                # Hier wäre jetzt eine Zählung der Teilnehmer für das Event und Speicherung im Event schön
+                # Besser wäre ein Custom Manager, der einfach bei
+                #  'Event.objects.with_counts() die Anzahl der Objekte zurück gäbe
+                #
+                #  Beispiel:
+                # ------- models.py ---------------
+                '''
+                from django.db import models
+
+                class PollManager(models.Manager):
+                    def with_counts(self):
+                        from django.db import connection
+                        with connection.cursor() as cursor:
+                            cursor.execute("""
+                                SELECT p.id, p.question, p.poll_date, COUNT(*)
+                                FROM polls_opinionpoll p, polls_response r
+                                WHERE p.id = r.poll_id
+                                GROUP BY p.id, p.question, p.poll_date
+                                ORDER BY p.poll_date DESC""")
+                            result_list = []
+                            for row in cursor.fetchall():
+                                p = self.model(id=row[0], question=row[1], poll_date=row[2])
+                                p.num_responses = row[3]
+                                result_list.append(p)
+                        return result_list
+
+                class OpinionPoll(models.Model):
+                    question = models.CharField(max_length=200)
+                    poll_date = models.DateField()
+                    objects = PollManager()
+
+                class Response(models.Model):
+                    poll = models.ForeignKey(OpinionPoll, on_delete=models.CASCADE)
+                    person_name = models.CharField(max_length=50)
+                    response = models.TextField()
+                '''
+                # ---------------------------------------------
+
+
                 if setts.senden:
                     """
                       Anmweldung war erfolgreich, sende nun die Emails
